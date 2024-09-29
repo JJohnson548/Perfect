262d969e96cedde54765ab81670062660208cc42
johns <johns@JONSCOMPUTER>
2024-09-29 18:58:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Perfect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a number to determine if it is a perfect numebr");
+
+            int userNumber;
+
+            if (int.TryParse(Console.ReadLine(), out userNumber) && userNumber > 0)
+            {
+                int sumOfNumbers = 0;
+
+                for (int i = 1; i < userNumber; i++)
+                {
+                    if (userNumber % i == 0)
+                        sumOfNumbers += i; 
+                }
+
+                if (sumOfNumbers == userNumber)
+                    Console.WriteLine ("perfect number.");
+                else
+                    Console.WriteLine("not a perfect number.");
+            }
+            else
+            {
+                Console.WriteLine("Not a valid integer");
+       
+        }
+    }
+}     }
